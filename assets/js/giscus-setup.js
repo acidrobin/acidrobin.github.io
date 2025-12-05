d7b339507d1a1b207abef829c12d736518697340
acidrobin <acidrobin@users.noreply.github.com>
2025-12-05 17:52:45 +0000

    Deploying to gh-pages from @ acidrobin/acidrobin.github.io@9ba4d9e8b32ab17c797c1ef4cd8d7512627a6c20 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "acidrobin/al-folio",
+    "data-repo": "acidrobin/acidrobin.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
